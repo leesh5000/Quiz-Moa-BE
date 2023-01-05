997e719d2fa5372ef887e78d0c03b07875e9aa66
leesh5000 <leesh50000@gmail.com>
2023-01-06 08:58:02 +0900

    #10 - 스프링 시큐리티 예외처리 로직 수정
    복잡한 클래스 파일을 줄이고자 기존의 클래스 방식에서 ApplicationConfiguration 에서 빈을 주입하는 방식으로 변경하였습니다.

--- src/main/java/com/leesh/quiz/config/ApplicationConfiguration.java
+++ src/main/java/com/leesh/quiz/config/ApplicationConfiguration.java
@@ -1,8 +1,10 @@
 package com.leesh.quiz.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leesh.quiz.domain.user.User;
 import com.leesh.quiz.domain.user.UserRepository;
 import com.leesh.quiz.security.CustomUserDetails;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +16,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.util.HashMap;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
 @Configuration
 public class ApplicationConfiguration {
 
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -50,4 +59,42 @@ public class ApplicationConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * 토큰이 유효하지 않거나, 유저의 비밀번호가 틀리거나 등등
+     */
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+
+        return (request, response, exception) -> {
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            var body = new HashMap<>();
+            body.put("error", exception.getMessage());
+
+            objectMapper.writeValue(response.getWriter(), body);
+        };
+
+    }
+
+    /**
+     * 권한이 없는 경우
+     */
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+
+        return (request, response, exception) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            var body = new HashMap<>();
+            body.put("error", exception.getMessage());
+
+            objectMapper.writeValue(response.getWriter(), body);
+        };
+
+    }
 }
