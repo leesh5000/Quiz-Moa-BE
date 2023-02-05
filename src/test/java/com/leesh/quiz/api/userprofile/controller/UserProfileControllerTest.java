fb2dac8688af5c04e28116562e2dbb17d0518d64
leesh5000 <leesh50000@gmail.com>
2023-02-06 03:56:36 +0900

    #61 - 유저 퀴즈 목록 조회 DTO 수정
    - 퀴즈 목록 조회 DTO에 퀴즈 내용 필드를 제거하였습니다. 왜냐하면, 어차피 목록 조회 API는 퀴즈 목록 화면을 위한 API 이기 때문에 그 세부 내용인 퀴즈 내용까지 보여줄 필요는 없습니다. 해당 목록을 통해 퀴즈 세부 조회를 한다면, 그 때 해당 퀴즈의 내용을 보여주는 것이 성능상으로 더 좋다고 판단하였습니다.

--- src/test/java/com/leesh/quiz/api/userprofile/controller/UserProfileControllerTest.java
+++ src/test/java/com/leesh/quiz/api/userprofile/controller/UserProfileControllerTest.java
@@ -75,9 +75,9 @@ class UserProfileControllerTest {
                 .willReturn(UserInfo.of(userId, Role.USER));
 
         List<MyQuizDto> content = List.of(
-                new MyQuizDto(1L, "HTTP 프로토콜의 특징은 무엇인가요?", "특징이랑 이유까지 함께 답해주세요.", 5, "test1@gmail.com", 12, LocalDateTime.now(), LocalDateTime.now()),
-                new MyQuizDto(2L, "RDB와 NoSQL의 차이점은 무엇인가?", "추가적으로 각 DB의 장단점과 종류에 대해서도 답해주세요.", 0, "test1@gmail.com", 3, LocalDateTime.now(), LocalDateTime.now()),
-                new MyQuizDto(3L, "컴파일 언어와 스크립트 언어의 차이점은?", "제목과 동일", 2, "test1@gmail.com", 2, LocalDateTime.now(), LocalDateTime.now())
+                new MyQuizDto(1L, "HTTP 프로토콜의 특징은 무엇인가요?", 5, "test1@gmail.com", 12, LocalDateTime.now(), LocalDateTime.now()),
+                new MyQuizDto(2L, "RDB와 NoSQL의 차이점은 무엇인가?", 0, "test1@gmail.com", 3, LocalDateTime.now(), LocalDateTime.now()),
+                new MyQuizDto(3L, "컴파일 언어와 스크립트 언어의 차이점은?", 2, "test1@gmail.com", 2, LocalDateTime.now(), LocalDateTime.now())
                 );
 
         int totalElements = 1125;
@@ -102,7 +102,6 @@ class UserProfileControllerTest {
                 .andExpect(jsonPath("$.content").isArray())
                 .andExpect(jsonPath("$.content[0].id").value(1L))
                 .andExpect(jsonPath("$.content[0].title").value("HTTP 프로토콜의 특징은 무엇인가요?"))
-                .andExpect(jsonPath("$.content[0].contents").value("특징이랑 이유까지 함께 답해주세요."))
                 .andExpect(jsonPath("$.content[0].answerCount").value(5))
                 .andExpect(jsonPath("$.content[0].author").value("test1@gmail.com"))
                 .andExpect(jsonPath("$.content[0].votes").value(12))
@@ -135,7 +134,6 @@ class UserProfileControllerTest {
                                 fieldWithPath("content").description("퀴즈 목록"),
                                 fieldWithPath("content[].id").description("퀴즈 ID (PK값)"),
                                 fieldWithPath("content[].title").description("퀴즈 제목"),
-                                fieldWithPath("content[].contents").description("퀴즈 내용"),
                                 fieldWithPath("content[].answerCount").description("퀴즈 답변 수"),
                                 fieldWithPath("content[].author").description("퀴즈 작성자"),
                                 fieldWithPath("content[].votes").description("이 퀴즈가 얻은 추천 수 (음수도 가능)"),
@@ -187,7 +185,7 @@ class UserProfileControllerTest {
 
         then(tokenService).should(times(1)).extractUserInfo(any(String.class));
         then(userProfileService).should(times(1)).editMyQuiz(any(EditMyQuizDto.Request.class), any(UserInfo.class), anyLong());
-        
+
         // API 문서화
         result
                 .andDo(document("user-profile/edit-my-quiz",
