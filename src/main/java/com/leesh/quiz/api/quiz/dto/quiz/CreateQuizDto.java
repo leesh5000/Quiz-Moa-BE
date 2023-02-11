e692d85a25fff362db088c1a008450bf037ebb64
leesh5000 <leesh50000@gmail.com>
2023-02-11 17:08:39 +0900

    #64 - 퀴즈 컨텐츠 유효성 검증 추가

--- src/main/java/com/leesh/quiz/api/quiz/dto/quiz/CreateQuizDto.java
+++ src/main/java/com/leesh/quiz/api/quiz/dto/quiz/CreateQuizDto.java
@@ -1,6 +1,7 @@
 package com.leesh.quiz.api.quiz.dto.quiz;
 
 import com.leesh.quiz.domain.quiz.Quiz;
+import com.leesh.quiz.domain.quiz.validation.QuizContents;
 import com.leesh.quiz.domain.quiz.validation.QuizTitle;
 import com.leesh.quiz.domain.user.User;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class CreateQuizDto {
 
-    public record Request(@QuizTitle String title, String contents) {
+    public record Request(@QuizTitle String title, @QuizContents String contents) {
 
         public Quiz toEntity(User user) {
             return Quiz.of(title, contents, user);
