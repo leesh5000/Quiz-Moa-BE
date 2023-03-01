b572223d41ec8ea183b2482776e65c5bd3ed6b33
leesh5000 <leesh50000@gmail.com>
2023-03-01 17:45:06 +0900

    update actions.yml

--- src/test/java/com/leesh/quiz/testconfiguration/MvcTestConfiguration.java
+++ src/test/java/com/leesh/quiz/testconfiguration/MvcTestConfiguration.java
@@ -3,8 +3,6 @@ package com.leesh.quiz.testconfiguration;
 import com.leesh.quiz.global.jwt.service.JwtTokenService;
 import com.leesh.quiz.global.xss.HtmlCharacterEscapes;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Import;
 
 @TestConfiguration
@@ -12,8 +10,5 @@ import org.springframework.context.annotation.Import;
         HtmlCharacterEscapes.class,
         RestDocsConfiguration.class})
 public class MvcTestConfiguration {
-
-    @MockBean
-    MessageSource messageSource;
 
 }
