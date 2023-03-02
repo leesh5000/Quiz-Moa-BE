9963c7c6cf54bf98ce12d27ef9ce55e5a1229ddf
leesh5000 <leesh50000@gmail.com>
2023-03-03 02:38:41 +0900

    #73 - 테스트 실패 수정

--- src/test/java/com/leesh/quiz/testconfiguration/MvcTestConfiguration.java
+++ src/test/java/com/leesh/quiz/testconfiguration/MvcTestConfiguration.java
@@ -1,19 +1,26 @@
 package com.leesh.quiz.testconfiguration;
 
+import com.leesh.quiz.global.configuration.LocaleConfiguration;
+import com.leesh.quiz.global.configuration.MessageConfiguration;
 import com.leesh.quiz.global.jwt.service.JwtTokenService;
 import com.leesh.quiz.global.xss.HtmlCharacterEscapes;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+
+import java.util.Locale;
 
 @TestConfiguration
 @Import({JwtTokenService.class,
         HtmlCharacterEscapes.class,
-        RestDocsConfiguration.class})
+        RestDocsConfiguration.class,
+        MessageConfiguration.class
+})
 public class MvcTestConfiguration {
 
-    @MockBean
-    MessageSource messageSource;
+    @Bean
+    public LocaleConfiguration localeConfiguration() {
+        return new LocaleConfiguration(Locale.getDefault().toString());
+    }
 
 }
