08e5c22fce2f0bf8862280eda62f7837b632c267
leesh5000 <leesh50000@gmail.com>
2023-03-07 08:25:07 +0900

    #86 - 제공받은 이메일 중 앞부분을 기본 닉네임으로 설정

--- src/main/java/com/leesh/quiz/external/oauth2/kakao/dto/KakaoUserInfoDto.java
+++ src/main/java/com/leesh/quiz/external/oauth2/kakao/dto/KakaoUserInfoDto.java
@@ -18,13 +18,12 @@ public record KakaoUserInfoDto(String id,
 
     @Override
     public String getName() {
-        return kakaoAccount.profile.nickname;
+        return !StringUtils.hasText(kakaoAccount.email) ? id : kakaoAccount.email.split("@")[0];
     }
 
     @Override
     public String getEmail() {
-        return !StringUtils.hasText(kakaoAccount.email) ?
-                id : kakaoAccount.email;
+        return !StringUtils.hasText(kakaoAccount.email) ? id : kakaoAccount.email;
     }
 
     @Override
